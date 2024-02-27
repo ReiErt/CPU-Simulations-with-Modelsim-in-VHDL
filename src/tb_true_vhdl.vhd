7e5c75f589c32740bd4dd2196189b2fdb799e2e7
Reilly E <125475072+ReiErt@users.noreply.github.com>
2024-02-27 15:52:57 +0100

    Update tb_true_vhdl.vhd

--- src/tb_true_vhdl.vhd
+++ src/tb_true_vhdl.vhd
@@ -6,7 +6,6 @@ use ieee.std_logic_textio.all;
 use ieee.math_real.all;
 
 library work;
---use work.file_io_pckg.all;
 use work.type_definitions_pckg.all;
 use work.function_pckg.all;
 
@@ -67,37 +66,7 @@ begin
       wait until (sl_tb_vld_out = '1');
     end loop;
   end loop;
-
-
---    -- reset stuff
---    sl_rst_in               <= '1';
---    sl_tb_vld_in            <= '0';
---    sv_tb_data_in           <= (others => '0');
---    wait for 10 us;
---    wait until (rising_edge(sl_clk_in));
---    sl_rst_in               <= '0';
---    wait until (rising_edge(sl_clk_in));
---
---
---    for m in 0 to 255 loop
---      sl_tb_vld_in          <= '1';
---      --sv_tb_data_in       <= "00111010";
---      sv_tb_data_in         <= std_logic_vector(to_unsigned(m, sv_tb_data_in'length));
---      wait until (rising_edge(sl_clk_in));
---      sl_tb_vld_in          <= '0';
---      wait until (rising_edge(sl_tb_rdy_out));
---    end loop;
---
---    for n in 0 to 255 loop
---      sl_tb_vld_in          <= '1';
---      --sv_tb_data_in       <= "00111010";
---      sv_tb_data_in         <= std_logic_vector(to_unsigned(n, sv_tb_data_in'length));
---      wait until (rising_edge(sl_clk_in));
---      sl_tb_vld_in          <= '0';
---      wait until (rising_edge(sl_tb_rdy_out));
---    end loop;
-
-    wait;
+  wait;
   end process;
 
 -- Instance Area
